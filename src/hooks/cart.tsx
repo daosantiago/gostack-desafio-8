542e97ba816865bbeba60b2d6ff1fc98139b5779
Anderson <daosantiago@gmail.com>
2022-09-17 15:26:59 +0100

    add products to cart

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -30,15 +30,35 @@ const CartProvider: React.FC = ({ children }) => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const storagedProducts = await AsyncStorage.getItem(
+        '@GoMarketplace:products',
+      );
+
+      if (storagedProducts) {
+        setProducts([...JSON.parse(storagedProducts)]);
+      }
     }
 
     loadProducts();
   }, []);
 
-  const addToCart = useCallback(async product => {
-    // TODO ADD A NEW ITEM TO THE CART
-  }, []);
+  const addToCart = useCallback(
+    async product => {
+      const productExist = products.find(prod => prod.id === product.id);
+
+      if (productExist) {
+        const newProducts = products.map(prod => {
+          if (prod.id === product.id)
+            return { ...prod, quantity: prod.quantity + 1 };
+          return prod;
+        });
+        setProducts(newProducts);
+      } else {
+        setProducts([...products, { ...product, quantity: 1 }]);
+      }
+    },
+    [products],
+  );
 
   const increment = useCallback(async id => {
     // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
